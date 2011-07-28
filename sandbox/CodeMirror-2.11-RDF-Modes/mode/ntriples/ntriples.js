341eda615945be3b1fa31ccce5aeb570c4877572
michele.mostarda <michele.mostarda@23f94136-cd2d-11dd-bfe9-7982af55821d>
2011-07-28 08:34:26 +0000

    Added patch for issue 174.

--- sandbox/CodeMirror-2.11-RDF-Modes/mode/ntriples/ntriples.js
+++ sandbox/CodeMirror-2.11-RDF-Modes/mode/ntriples/ntriples.js
@@ -1,14 +1,54 @@
-CodeMirror.defineMode("ntriples", function() {
+Location = {
+      BEGIN     : 0,
+      SUB_URI   : 1,
+      SUB_BNODE : 2,
+      SUB_PRE   : 3,
+      PRED      : 4,
+      PRED_OBJ  : 5,
+      OBJ_URI   : 6,
+      OBJ_BNODE : 7,
+      OBJ_LIT   : 8,
+      POST_OBJ  : 9,
+      ERROR     : 10
+};
+function transitState(currState, c) {
+    var currLocation = currState.location;
+    var ret;
+    
+    if( currLocation == Location.SUB_URI       && c == '>') ret = Location.SUB_PRE;
+    else if(currLocation == Location.SUB_BNODE && c == ' ') ret = Location.SUB_PRE;
+    else if(currLocation == Location.PRED      && c == '>') ret = Location.PRED_OBJ;
+    else if(currLocation == Location.OBJ_URI   && c == '>') ret = Location.POST_OBJ;
+    else if(currLocation == Location.OBJ_BNODE && c == ' ') ret = Location.POST_OBJ;
+    else if(currLocation == Location.OBJ_LIT   && c == '"') ret = Location.POST_OBJ;    
+    
+    else if(currLocation == Location.BEGIN && c == '<') ret = Location.SUB_URI;
+    else if(currLocation == Location.BEGIN && c == '_') ret = Location.SUB_BNODE;
+    else if(currLocation == Location.SUB_PRE          ) ret = Location.PRED;
+    else if(currLocation == Location.PRED_OBJ && c == '<') ret = Location.OBJ_URI;
+    else if(currLocation == Location.PRED_OBJ && c == '_') ret = Location.OBJ_BNODE;
+    else if(currLocation == Location.PRED_OBJ && c == '"') ret = Location.OBJ_LIT;
+    else if(
+        ( currLocation == Location.OBJ_URI || currLocation == Location.OBJ_LIT || currLocation == Location.OBJ_BNODE )
+        &&
+        c == '.'
+    ) ret = Location.BEGIN;    
+    else ret = Location.ERROR;
+    
+    console.log('next location: ' + ret + ' curr location: ' + currLocation);
+    currState.location=ret;
+}
+CodeMirror.defineMode("ntriples", function() {  
   untilSpace  = function(c) { return c != ' '; };
   untilEndURI = function(c) { return c != '>'; };
   return {
     startState: function() {
-       return {inuri : false};
+       return { location : Location.BEGIN, inside : false };
     },
     token: function(stream, state) {
       var ch = stream.next();
       if(ch == '<') {
-         state.inuri=true;
+         transitState(state, ch);
          stream.eatWhile( function(c) { return c != '#' && c != '>'; } );
          if( stream.match('#', false) ) return 'uri';
          stream.eatWhile(untilEndURI);
@@ -18,26 +58,32 @@ CodeMirror.defineMode("ntriples", function() {
       if(ch == '#') {
         stream.eatWhile(untilEndURI);
         return 'uri-anchor';
-     }
+      }
       if(ch == '>') {
-         state.inuri=false; 
+         transitState(state, ch);  
          stream.eatWhile(untilSpace);
          return 'uri';
       }
       if(ch == '_') {
+          transitState(state, ch);
           stream.eatWhile(untilSpace);
+          transitState(state, ' ');
           return 'bnode';
       }
       if(ch == '"') {
+          transitState(state, ch);
           stream.eatWhile( function(c) { return c != '"' }  );
+          transitState(state, '"');
           stream.next();
           return 'literal';
       }
       if( ch == '@' ) {
           stream.eatWhile(untilSpace);
+          transitState(state, ' ');
           return 'literal-lang';
       }
       if( ch == '.' ) {
+          transitState(state, ch);
           stream.skipToEnd();
       }
     }
