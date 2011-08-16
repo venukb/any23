52f2a567ad374a5ad58d1c5027e2f541574464df
michele.mostarda <michele.mostarda@23f94136-cd2d-11dd-bfe9-7982af55821d>
2011-08-16 12:59:08 +0000

    Completed NTriples detection and state management.

--- sandbox/CodeMirror-2.11-RDF-Modes/mode/ntriples/ntriples.js
+++ sandbox/CodeMirror-2.11-RDF-Modes/mode/ntriples/ntriples.js
@@ -1,41 +1,63 @@
+/* 
+    pre_subject ->
+        ( writing_subject_uri | writing_bnode_uri )
+            -> pre_predicate 
+                -> writing_predicate_uri 
+                    -> pre_object 
+                        -> writing_object_uri | writing_object_bnode | 
+                          ( 
+                            writing_object_literal 
+                                -> writing_literal_lang | writing_literal_type
+                          )
+                            -> post_object
+                                -> BEGIN
+*/
 Location = {
-      BEGIN     : 0,
-      SUB_URI   : 1,
-      SUB_BNODE : 2,
-      SUB_PRE   : 3,
-      PRED      : 4,
-      PRED_OBJ  : 5,
-      OBJ_URI   : 6,
-      OBJ_BNODE : 7,
-      OBJ_LIT   : 8,
-      POST_OBJ  : 9,
-      ERROR     : 10
+      PRE_SUBJECT         : 0,
+      WRITING_SUB_URI     : 1,
+      WRITING_BNODE_URI   : 2,
+      PRE_PRED            : 3,
+      WRITING_PRED_URI    : 4,
+      PRE_OBJ             : 5,
+      WRITING_OBJ_URI     : 6,
+      WRITING_OBJ_BNODE   : 7,
+      WRITING_OBJ_LITERAL : 8,
+      WRITING_LIT_LANG    : 9,
+      WRITING_LIT_TYPE    : 10,
+      POST_OBJ            : 11,
+      ERROR               : 12
 };
 function transitState(currState, c) {
     var currLocation = currState.location;
     var ret;
     
-    if( currLocation == Location.SUB_URI       && c == '>') ret = Location.SUB_PRE;
-    else if(currLocation == Location.SUB_BNODE && c == ' ') ret = Location.SUB_PRE;
-    else if(currLocation == Location.PRED      && c == '>') ret = Location.PRED_OBJ;
-    else if(currLocation == Location.OBJ_URI   && c == '>') ret = Location.POST_OBJ;
-    else if(currLocation == Location.OBJ_BNODE && c == ' ') ret = Location.POST_OBJ;
-    else if(currLocation == Location.OBJ_LIT   && c == '"') ret = Location.POST_OBJ;    
+    // Openings.
+    if     (currLocation == Location.PRE_SUBJECT && c == '<') ret = Location.WRITING_SUB_URI;
+    else if(currLocation == Location.PRE_SUBJECT && c == '_') ret = Location.WRITING_BNODE_URI;
+    else if(currLocation == Location.PRE_PRED && c == '<')    ret = Location.WRITING_PRED_URI;
+    else if(currLocation == Location.PRE_OBJ  && c == '<')    ret = Location.WRITING_OBJ_URI;
+    else if(currLocation == Location.PRE_OBJ  && c == '_')    ret = Location.WRITING_OBJ_BNODE;
+    else if(currLocation == Location.PRE_OBJ  && c == '"')    ret = Location.WRITING_OBJ_LITERAL;
     
-    else if(currLocation == Location.BEGIN && c == '<') ret = Location.SUB_URI;
-    else if(currLocation == Location.BEGIN && c == '_') ret = Location.SUB_BNODE;
-    else if(currLocation == Location.SUB_PRE          ) ret = Location.PRED;
-    else if(currLocation == Location.PRED_OBJ && c == '<') ret = Location.OBJ_URI;
-    else if(currLocation == Location.PRED_OBJ && c == '_') ret = Location.OBJ_BNODE;
-    else if(currLocation == Location.PRED_OBJ && c == '"') ret = Location.OBJ_LIT;
-    else if(
-        ( currLocation == Location.OBJ_URI || currLocation == Location.OBJ_LIT || currLocation == Location.OBJ_BNODE )
-        &&
-        c == '.'
-    ) ret = Location.BEGIN;    
+    // Closing.
+    else if(currLocation == Location.WRITING_SUB_URI   && c == '>') ret = Location.PRE_PRED;
+    else if(currLocation == Location.WRITING_BNODE_URI && c == ' ') ret = Location.PRE_PRED;
+    else if(currLocation == Location.WRITING_PRED_URI  && c == '>') ret = Location.PRE_OBJ;
+    else if(currLocation == Location.WRITING_OBJ_URI     && c == '>') ret = Location.POST_OBJ; // TODO: space could not pe specified.
+    else if(currLocation == Location.WRITING_OBJ_BNODE   && c == ' ') ret = Location.POST_OBJ; // TODO: space could not pe specified.
+    else if(currLocation == Location.WRITING_OBJ_LITERAL && c == '"') ret = Location.POST_OBJ; // TODO: space could not pe specified.
+    
+    else if(currLocation == Location.WRITING_OBJ_LITERAL && c == '@') ret = Location.WRITING_LIT_LANG;
+    else if(currLocation == Location.WRITING_OBJ_LITERAL && c == '^') ret = Location.WRITING_LIT_TYPE;
+    else if(currLocation == Location.WRITING_LIT_LANG || currLocation == Location.WRITING_LIT_TYPE && c == ' ') ret = Location.POST_OBJ;
+    
+    // Reset.
+    else if(currLocation == Location.POST_OBJ && c == '.') ret = Location.PRE_SUBJECT;    
+    
+    // Error
     else ret = Location.ERROR;
     
-    console.log('next location: ' + ret + ' curr location: ' + currLocation);
+    console.log('char ' + c + ' curr location: ' + currLocation + ' next location: ' + ret);
     currState.location=ret;
 }
 CodeMirror.defineMode("ntriples", function() {  
@@ -43,7 +65,7 @@ CodeMirror.defineMode("ntriples", function() {
   untilEndURI = function(c) { return c != '>'; };
   return {
     startState: function() {
-       return { location : Location.BEGIN, inside : false };
+       return { location : Location.PRE_SUBJECT, inside : false };
     },
     token: function(stream, state) {
       var ch = stream.next();
@@ -53,38 +75,49 @@ CodeMirror.defineMode("ntriples", function() {
          if( stream.match('#', false) ) return 'uri';
          stream.eatWhile(untilEndURI);
          stream.next();
+         transitState(state, '>');
          return 'uri';
       }
       if(ch == '#') {
         stream.eatWhile(untilEndURI);
+        stream.next();
+        transitState(state, '>');
         return 'uri-anchor';
-      }
-      if(ch == '>') {
-         transitState(state, ch);  
-         stream.eatWhile(untilSpace);
-         return 'uri';
       }
       if(ch == '_') {
           transitState(state, ch);
           stream.eatWhile(untilSpace);
+          stream.next();
           transitState(state, ' ');
           return 'bnode';
       }
       if(ch == '"') {
           transitState(state, ch);
-          stream.eatWhile( function(c) { return c != '"' }  );
-          transitState(state, '"');
+          stream.eatWhile( function(c) { return c != '"'; } );
           stream.next();
+          if( stream.peek() != '@' && stream.peek() != '^' ) {
+              transitState(state, '"');
+          }
           return 'literal';
       }
       if( ch == '@' ) {
+          stream.next();
+          transitState(state, '@');
           stream.eatWhile(untilSpace);
+          stream.next();
           transitState(state, ' ');
           return 'literal-lang';
       }
+      if( ch == '^' ) {
+          stream.next();
+          transitState(state, '^');
+          stream.eatWhile(untilSpace);
+          stream.next();
+          transitState(state, ' ');
+          return 'literal-type';
+      }
       if( ch == '.' ) {
           transitState(state, ch);
-          stream.skipToEnd();
       }
     }
   };
